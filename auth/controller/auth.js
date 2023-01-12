b659ff199a4230df81d6a004ef6fd9a830921636
Kwameasiago <kwameasiago@gmail.com>
2023-01-12 23:29:14 +0300

    auth service Created login and register route

--- auth/controller/auth.js
+++ auth/controller/auth.js
@@ -1,20 +1,29 @@
 import express from 'express';
 import Authentication from '../services/auth';
+
 const userRoute = express.Router();
 
 const auth = new Authentication();
-const { register } = auth;
+const { register, login, userInfo } = auth;
 
 userRoute.post('/signup', async (req, res) => {
     const {body} = req;
     res.status(200).send({
-        hello: await  register(body)
+        ... await  register(body)
     })
 });
 
-userRoute.post('/signin', (req, res) => {
+userRoute.post('/signin', async (req, res) => {
+    const {body} = req;
     res.status(200).send({
-        res: 'hello world from sign in'
+        ... await  login(body)
+    })
+});
+
+userRoute.get('/info', async (req, res) => {
+    const {headers} = req;
+    res.status(200).send({
+        ... await  userInfo(headers)
     })
 });
 
--- auth/services/auth.js
+++ auth/services/auth.js
@@ -1,19 +1,72 @@
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcrypt';
 import db from '../models';
-
+import jsonwebtoken from 'jsonwebtoken';
 
 class Authentication {
     async register(userData) {
         const { firstName, lastName, email, password } = userData;
         try {
+            console.log(process.env.SALT)
             let res = await db.User.create({
                 firstName, lastName, email,
-                password: await bcrypt.hash(password, process.env.SALT || 10)
+                password: await bcrypt.hash(password, 10)
             });
-            return res
+            let jwt
+            jwt = await jsonwebtoken.sign({ firstName, lastName, email}, process.env.SECRET_KEY, { expiresIn: '7d' });
+
+            return {jwt}
         } catch (error) {
             console.log(error)
-            return error
+            return {jwt: ''}
+        }
+    }
+
+    async login(userData){
+        const { email, password: plainPassword } = userData;
+        try {
+            let user = await db.User.findOne({
+                where: {email}
+            });
+            if(!user){
+                return {jwt: ''}
+            }
+            const {dataValues:{password, firstName, lastName, email:useEmail}} = user
+
+            let compare  = await bcrypt.compare(plainPassword, password);
+            let jwt;
+            if(compare){
+                console.log('----',process.env.SECRET_KEY);
+                jwt = await jsonwebtoken.sign({ firstName, lastName, useEmail}, process.env.SECRET_KEY, { expiresIn: '7d' });
+            }else{
+                jwt = {jwt: ''}
+            }
+    
+            return {jwt}
+        } catch (error) {
+            console.log(error)
+            return {jwt: ''}
+        }
+    }
+
+    async userInfo(token){
+        
+        try {
+            console.log(token);
+            let decoded = jsonwebtoken.verify(token['auth-token'], process.env.SECRET_KEY);
+
+            // let user = await db.User.findOne({
+            //     where: {email}
+            // });
+            
+            return decoded
+            
+        } catch (error) {
+            
+            return {
+                name: "JsonWebTokenError",
+                message: "invalid signature",
+                error
+            }
         }
     }
 }
